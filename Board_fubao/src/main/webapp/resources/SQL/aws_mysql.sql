dc0b2304da43dfcd36ed554a419c591861aa8e50
realsoly <jk120926@gmail.com>
2024-07-05 18:58:44 +0900

    [style] db update 코드 수정없음

--- Board_fubao/src/main/webapp/resources/SQL/aws_mysql.sql
+++ Board_fubao/src/main/webapp/resources/SQL/aws_mysql.sql
@@ -14,15 +14,15 @@ select @@global.time_zone, @@session.time_zone;
 
 show tables;
 
-select *
-from TBL_MEMBER;
-
 show variables like 'lower%';
 
-EXPLAIN TBL_MEMBER;
+
+
+alter table tbl_board modify b_like int default 0;
+commit;
 
 -- ** 회원 테이블 ** --
-CREATE TABLE TBL_MEMBER (
+CREATE TABLE tbl_member (
 	id          VARCHAR(40) NOT NULL, -- 회원ID
 	nickname    VARCHAR(100) NOT NULL, -- 닉네임
 	name        VARCHAR(20) NOT NULL, -- 이름 
@@ -39,27 +39,26 @@ CREATE TABLE TBL_MEMBER (
 );
 
 -- ** 게시글 테이블 ** --
-CREATE TABLE TBL_BOARD (
+CREATE TABLE tbl_board(
 	b_idx     INT   auto_increment   NOT NULL, -- 글번호
 	id_fk     VARCHAR(40)    NOT NULL, -- 회원ID
-    NICKNAME  VARCHAR(100)   NOT NULL,
+    nickname  VARCHAR(100)   NOT NULL,
 	category_idx_fk  INT   NOT NULL, -- 카테고리번호
 	b_title   VARCHAR(200)   NOT NULL, -- 글제목
 	b_content VARCHAR(4000)  NOT NULL, -- 글내용
 	b_date    timestamp   default now()  NOT NULL, -- 글등록일
-	b_like    INT        NOT NULL, -- 추천수
-	b_hit     INT       NOT NULL,  -- 조회수
+	b_like    INT       default 0 NOT NULL, -- 추천수
+	b_hit     INT       default 0 NOT NULL,  -- 조회수
     b_status  INT       default 1 NOT NULL, -- 글 삭제여부 0 또는 1
     c_cnt     INT       default 0 NOT NULL, -- 댓글 개수
     constraint BOARD_PK primary key(b_idx),
-    constraint BOARD_FK_1 foreign key(id_fk) references TBL_MEMBER(id),
-    constraint BOARD_FK_2 foreign key(category_idx_fk) references TBL_CATEGORY(category_idx),
+    constraint BOARD_FK_1 foreign key(id_fk) references tbl_member(id),
+    constraint BOARD_FK_2 foreign key(category_idx_fk) references tbl_category(category_idx),
     constraint BOARD_CK_1 check( b_status in(1,0) )
 );
 
-
 -- ** 댓글 테이블 ** --
-CREATE TABLE TBL_COMMENT (
+CREATE TABLE tbl_comment (
 	c_idx     INT    auto_increment     NOT NULL, -- 댓글번호
 	id_fk     VARCHAR(40)   NOT NULL,     -- 회원ID
 	b_idx_fk  INT        NOT NULL,     -- 글번호
@@ -71,41 +70,26 @@ CREATE TABLE TBL_COMMENT (
     org_file_name VARCHAR(255) null,
     file_size   INT      null
     ,constraint COMMENT_PK primary key(c_idx)
-    ,constraint COMMENT_FK_1 foreign key(id_fk) references TBL_MEMBER(id)
-    ,constraint COMMENT_FK_2 foreign key(b_idx_fk) references TBL_BOARD(b_idx) on delete cascade
+    ,constraint COMMENT_FK_1 foreign key(id_fk) references tbl_member(id)
+    ,constraint COMMENT_FK_2 foreign key(b_idx_fk) references tbl_board(b_idx) on delete cascade
     ,constraint COMMENT_CK check( c_status in(1,0) ) 
 );
 
 
 -- ** 카테고리 테이블 ** --
-CREATE TABLE TBL_CATEGORY (
+CREATE TABLE tbl_category (
 	category_idx  INT  auto_increment  NOT NULL, -- 카테고리번호
 	category_name VARCHAR(80) NOT NULL,  -- 카테고리명
     constraint CATEGORY_PK primary key(category_idx)
 );
 
 -- ** 게시글 좋아요 테이블** --
-CREATE TABLE TBL_LIKE(
+CREATE TABLE tbl_like(
     id_fk        varchar(40) not null          -- 아이디
     ,b_idx_fk    INT not null                -- 글 번호
     ,constraint  tbl_like_pk primary key(id_fk, b_idx_fk)  
-    ,constraint  tbl_like_fk foreign key(id_fk) references TBL_MEMBER(id)
-    ,constraint  tbl_like_fk_2 foreign key(b_idx_fk) references TBL_BOARD(b_idx) on delete cascade
+    ,constraint  tbl_like_fk foreign key(id_fk) references tbl_member(id)
+    ,constraint  tbl_like_fk_2 foreign key(b_idx_fk) references tbl_board(b_idx) on delete cascade
 );
 
 
-insert into TBL_CATEGORY (category_name) values  ('가입인사');
-insert into TBL_CATEGORY (category_name) values  ('공지사항');
-insert into TBL_CATEGORY (category_name) values  ('BAO Family 소식');
-insert into TBL_CATEGORY (category_name) values  ('사육사와의 소통');
-insert into TBL_CATEGORY (category_name) values  ('판다월드 관람/체험 후기');
-insert into TBL_CATEGORY (category_name) values  ('자유게시판');
-insert into TBL_CATEGORY (category_name) values  ('꿀팁게시판');
-insert into TBL_CATEGORY (category_name) values  ('질문게시판');
-insert into TBL_CATEGORY (category_name) values  ('팬아트');
-insert into TBL_CATEGORY (category_name) values  ('인기글');
-
-commit;
-
-select *
-from TBL_CATEGORY;
